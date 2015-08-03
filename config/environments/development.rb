ed8adf0eaa9399b11a2f4489ec62e65f218c49de
Edward Park <parkes.edward@gmail.com>
2015-08-03 14:47:59 -0400

    added generic welcome controller and index view

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } #for devise
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
