0db32ad0194b73a7e205fd7db9b4da0d7ff95500
Edward Park <parkes.edward@gmail.com>
2015-08-04 10:16:30 -0400

    nested reminder routes to medications

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
-  resources :medications
+  resources :medications do
+    resources :reminders
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
