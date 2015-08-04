faf6b859de05ad9307e652d3a1302f8d7cb67f5c
Edward Park <parkes.edward@gmail.com>
2015-08-03 20:49:15 -0400

    created medication model and migrated table and established associaiton with users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803184819) do
+ActiveRecord::Schema.define(version: 20150804004654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "medications", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "dosage"
+    t.integer  "morning"
+    t.integer  "midday"
+    t.integer  "afternoon"
+    t.integer  "evening"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "medications", ["user_id"], name: "index_medications_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +49,5 @@ ActiveRecord::Schema.define(version: 20150803184819) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "medications", "users"
 end
