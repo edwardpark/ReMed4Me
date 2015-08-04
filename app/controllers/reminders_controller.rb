4fb074e58e26b462072b73fdc14396dbe202ef5d
Edward Park <parkes.edward@gmail.com>
2015-08-04 10:31:29 -0400

    added dependent: :destroy associations to all required models

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,5 +1,9 @@
 class RemindersController < ApplicationController
   def index
+    #create @morning_reminders
+    #create @midday_reminders
+    #create @afternoon_reminders
+    #create @evening_reminders 
 
   end
 
--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,4 +1,4 @@
 class Medication < ActiveRecord::Base
   belongs_to :user
-  has_many :reminders
+  has_many :reminders , dependent: :destroy
 end
