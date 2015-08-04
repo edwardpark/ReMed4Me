b9eed964080dec699990938094294a1d6899e82b
Edward Park <parkes.edward@gmail.com>
2015-08-04 14:57:07 -0400

    went back and seperated reminder instance to predetermined defaults

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -8,10 +8,12 @@ class MedicationsController < ApplicationController
   end
 
   def create
-    binding.pry
+    debugger
     @medication = current_user.medications.build(check_params)
-    @medication.save
-    binding.pry
+    @medication.save!
+  debugger
+    set_reminders(params[:reminders],@medication) #go through and create reminders ,@medication
+
     redirect_to @medication
   end
 
@@ -26,6 +28,7 @@ class MedicationsController < ApplicationController
   def update
     @medication = Medication.find(params[:id])
     if @medication.update( check_params )
+        #reset the reminders themsevles here.
         redirect_to @medication
     else
       render 'edit'
@@ -43,6 +46,15 @@ class MedicationsController < ApplicationController
   private
 
   def check_params
-    params.require(:medication).permit(:name,:description,:dosage,:morning,:midday,:afternoon,:evening)
+    params.require(:medication).permit(:name,:description,:dosage)
   end
+
+  def set_reminders(reminder_data, medication)
+    reminder_data.each do |interval, should_create_reminder|
+      medication.reminders.create(interval: interval) if should_create_reminder == "1"
+      medication.reminders.destroy(interval: interval) if should_create_reminder == "0" #will this throw a error if reminder doesn't exist?
+    end
+  end
+
+
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,3 +1,8 @@
 class Reminder < ActiveRecord::Base
   belongs_to :medication
+  def self.default_intervals #note how this seperates out the whole thing.
+    %w(morning midday afternoon evening)
+  end
+  #idea to just create seperate deafult intervals and use params instances to create them? 
+  #
 end
