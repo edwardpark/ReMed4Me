0d5bed73a0c7b5d6f808a17fc2a3773c54fb8b09
Edward Park <parkes.edward@gmail.com>
2015-08-05 17:06:41 -0400

    added instance variables for other views in welcomeController

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-
     @morning_reminders = Reminder.where("interval ='morning'")
-
-
-
+    @midday_reminders = Reminder.where("interval ='midday'")
+    @afternoon_reminders = Reminder.where("interval ='afternoon'")
+    @evening_reminders = Reminder.where("interval='evening'")
   end
 end
