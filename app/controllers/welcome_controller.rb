2bf8de807d8cf406c52b616ef3516ebc49b3e345
Edward Park <parkes.edward@gmail.com>
2015-08-05 16:41:58 -0400

    fixed welcome#index view to show medications of reminders matched to interval

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @morning_medication = current_user.medications.first #fix this!
-    #awesome so then find search returning all morning_medications..etc.
-    #and then in the look search and link to their reminders.
+
+    @morning_reminders = Reminder.where("interval ='morning'")
+
 
 
   end
