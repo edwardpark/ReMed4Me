91d5775d24dfbfc9a55f2bae411372aabff50296
Edward Park <parkes.edward@gmail.com>
2015-08-06 12:19:34 -0400

    bug. working on allowing empty fields in your homepage for new user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @morning_reminders = Reminder.where("interval ='morning'")
+    binding.pry
+    @morning_reminders = current_user.reminders.where("interval ='morning'")
     # @midday_reminders = Reminder.where("interval ='midday'")
     # @afternoon_reminders = Reminder.where("interval ='afternoon'")
     # @evening_reminders = Reminder.where("interval='evening'")
