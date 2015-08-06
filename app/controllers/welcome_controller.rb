f69066971db21325610a43ae8c2709b838988cbd
Edward Park <parkes.edward@gmail.com>
2015-08-05 20:04:17 -0400

    finally got paperclip working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!
   def index
     @morning_reminders = Reminder.where("interval ='morning'")
     @midday_reminders = Reminder.where("interval ='midday'")
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :medications, dependent: :destroy
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
